7baa57df888f4eae7ff6a5551c216a884fee4bab
Eduardo-byt <luizeduardomalta097@gmail.com>
2025-05-27 20:22:19 +0000

    criei o printf

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
-}
+int main f
+printf ("\n");
+// Realizando a movimentação da Rainha
+printf ("Digite um número de 1 à 8, representando a quantidade de casas que a Rainha vai mover: ");
+scanf ("%d"
+", &mov_Rainha);
+printf ("Digite a direção que o Rainha vai realizar, exemplo: (esquerda ou direita)-(Cima ou Baixo): \n*);
+scanf(" %[^\n]", direcao _Rainha);
+printf ("\n");
+printf(*** Realizando a movimentação da Rainha ***\n");
+do
+{
+printf ("Se movendo em %s1\n", direcao _Rainha);
+rainha++;
+} while (rainha < mov_Rainha);
+return 0;
